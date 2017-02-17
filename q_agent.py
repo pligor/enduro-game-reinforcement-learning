c6cc6bfb7dfc245b90a241f1b0555da0126efe1e
Georgios Pligoropoulos <george@pligor.com>
2017-02-17 17:08:09 +0000

    ok cool the q agent with the dummy states is working as expected

--- q_agent.py
+++ q_agent.py
@@ -13,24 +13,34 @@ class QAgent(Agent):
 
         states = np.zeros(2)
 
-        #TODO
-        self.bellmanQ = np.zeros((len(states), len(self.getActionsSet())))
+        Qshape = (len(states), len(self.getActionsSet()))
+
+        # TODO either set the e greedy policy to something else or give initial value functions
+        #here we are giving something larger than zero
+        #self.bellmanQ = np.zeros(Qshape)
+        self.bellmanQ = self.rng.rand(Qshape[0], Qshape[1])
 
         print "bellman q initially"
         print self.bellmanQ
 
-        self.curState = 0 #initially it is right (right is 0, left is 1)
+        self.curStateId = 0  # initially it is right (right is 0, left is 1)
 
         self.gamma = 0.9
 
+        self.cur_t = 1
+
     def isLeft(self, grid):
         row = grid[0, :]
-        half_ind = int(len(row)/2)
+        half_ind = int(len(row) / 2)
         return np.all(row[:half_ind] != 2)
 
     # def getActionById(self):
     #     """here the convention is that each action in the set"""
     #     pass
+    def updateQsa(self, stateId, actionId, value):
+        self.bellmanQ[stateId, actionId] = value
+        return self.bellmanQ[stateId, actionId]
+
     def getQsa(self, stateId, actionId):
         """it is trivial in this example but might not be like this in the future"""
         return self.bellmanQ[stateId, actionId]
@@ -39,20 +49,25 @@ class QAgent(Agent):
         """this is very simplified since the rows of bellmanQ are the state id"""
         return self.bellmanQ[stateId]
 
-    def __init__(self):
+    def __init__(self, rng):
         super(QAgent, self).__init__()
         # Add member variables to your class here
+        self.actionById = dict((k, v) for k, v in enumerate(self.getActionsSet()))
+        self.idByAction = dict((v, k) for k, v in self.actionById.iteritems())
 
-        print [Action.toString(a) for a in self.getActionsSet()]
+        print [(k, Action.toString(v)) for k, v in self.actionById.iteritems()]
 
-        self.actionById = dict( (k, v) for k, v in enumerate(self.getActionsSet()) )
-        self.idByAction = dict( (v, k) for k, v in self.actionById.iteritems() )
+        self.rng = rng
 
-        print self.idByAction
-
-        #print [(k, Action.toString(v)) for k, v in self.actionById.iteritems()]
+        self.lr_p_param = 0.51
+        assert self.lr_p_param > 0.5 and self.lr_p_param <= 1
 
         self.init()
+
+    def getNextLearningRate(self):
+        learningRate = 1. / np.power(self.cur_t, self.lr_p_param)
+        self.cur_t += 1
+        return learningRate
 
     def initialise(self, grid):
         """Called at the beginning of an episode. Use it to construct the initial state."""
@@ -63,9 +78,6 @@ class QAgent(Agent):
         an action. Remember to store the obtained reward.
         """
 
-        # You can get the set of possible actions and print it with:
-        # print [Action.toString(a) for a in self.getActionsSet()]
-
         # Execute the action and get the received reward signal
         # IMPORTANT NOTE:
         # 'action' must be one of the values in the actions set,
@@ -74,7 +86,7 @@ class QAgent(Agent):
         # self.total_reward += self.move(action)
 
         self.curAction = self.actionById[
-            np.argmax(self.getQbyS(self.curState))
+            np.argmax(self.getQbyS(self.curStateId))
         ]
 
         print Action.toString(self.curAction)
@@ -83,7 +95,7 @@ class QAgent(Agent):
 
         self.total_reward += self.curReward
 
-        time.sleep(2) #seconds
+        time.sleep(1)  # seconds
 
     def sense(self, grid):
         """ Constructs the next state from sensory signals.
@@ -92,10 +104,9 @@ class QAgent(Agent):
                 representation of the environment
         """
 
-        self.nextState = int(self.isLeft(grid))
+        self.nextStateId = int(self.isLeft(grid))
 
-        #print self.nextState
-        #print grid
+        print "next state id: %d" % self.nextStateId
 
         # Visualise the environment grid
         cv2.imshow("Environment Grid", EnvironmentState.draw(grid))
@@ -104,14 +115,23 @@ class QAgent(Agent):
         """ Performs the learning procudre. It is called after act() and
         sense() so you have access to the latest tuple (s, s', a, r).
         """
-        #self.curReward
-        #self.curState
-        #self.nextState
+        print self.bellmanQ
 
-        curQ = self.getQsa(self.curState, )
+        learningRate = self.getNextLearningRate()
 
-        self.curState = self.nextState
-        pass
+        curActionId = self.idByAction[self.curAction]
+
+        curQ = self.getQsa(self.curStateId, curActionId)
+
+        maxNextQ = np.max(self.getQbyS(self.nextStateId))
+
+        updateValue = curQ + learningRate*(self.curReward + self.gamma * maxNextQ - curQ)
+
+        self.updateQsa(self.curStateId, curActionId, updateValue)
+
+        self.curStateId = self.nextStateId
+
+        return
 
     def callback(self, learn, episode, iteration):
         """ Called at the end of each timestep for reporting/debugging purposes.
@@ -121,8 +141,15 @@ class QAgent(Agent):
         if not learn:
             cv2.imshow("Enduro", self._image)
             cv2.waitKey(40)
+        print
+
 
 if __name__ == "__main__":
-    a = QAgent()
-    a.run(True, episodes=2, draw=True)
+    seed = 16011984
+    rng = np.random.RandomState(seed=seed)
+
+    a = QAgent(rng=rng)
+
+    a.run(True, episodes=1, draw=True)
+
     print 'Total reward: ' + str(a.total_reward)
