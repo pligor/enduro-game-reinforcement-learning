ffb5080363a546faddbba2568eb576e10865d294
Georgios Pligoropoulos <george@pligor.com>
2017-02-19 12:17:39 +0000

    we have improved random agent by adding noop action as well

--- random_agent.py
+++ random_agent.py
@@ -7,22 +7,40 @@ import numpy as np
 #import matplotlib.pyplot as plt #it's not working
 
 class RandomAgent(Agent):
+    def getRewardInfo(self):
+        return np.array(self.totalRewards), np.array(self.rewardStreams)
+
+    def getActionsSet(self):
+        """including the noop action in our possible actions"""
+        return super(RandomAgent, self).getActionsSet() + [Action.NOOP]
+
     def __init__(self, rng):
         super(RandomAgent, self).__init__()
         # Add member variables to your class here
-        self.total_reward = 0
+        self.total_reward = None
+        self.reward_stream = None
         self.rng = rng
         #self.counter = 0
         print [Action.toString(a) for a in self.getActionsSet()]
+        print type(self.getActionsSet())
         #print len(self.getActionsSet())
 
+        self.totalRewards = []
+        self.rewardStreams = []
+
     def initialise(self, grid):
+        if self.total_reward is not None:
+            self.totalRewards.append(self.total_reward)
+        if self.reward_stream is not None:
+            self.rewardStreams.append(np.array(self.reward_stream))
+
         """ Called at the beginning of an episode. Use it to construct
         the initial state.
         """
         # Reset the total reward for the episode
         self.total_reward = 0
         print "new episode"
+        self.reward_stream = []
 
     def act(self):
         """ Implements the decision making process for selecting
@@ -46,10 +64,14 @@ class RandomAgent(Agent):
         # i.e. Action.LEFT, Action.RIGHT, Action.ACCELERATE or Action.BREAK
         # Do not use plain integers between 0 - 3 as it will not work
 
-        self.total_reward += self.move(action)
-        time.sleep(1)
+        cur_reward = self.move(action)
 
-    def isLeft(self, grid):
+        self.reward_stream.append(cur_reward)
+
+        self.total_reward += cur_reward
+
+    @staticmethod
+    def isLeft(grid):
         row = grid[0, :]
         half_ind = int(len(row)/2)
         return np.all(row[:half_ind] != 2)
@@ -60,8 +82,10 @@ class RandomAgent(Agent):
         grid -- 2-dimensional numpy array containing the latest grid
                 representation of the environment
         """
-        print grid[0, :]
-        print self.isLeft(grid)
+
+        #print grid[0, :]
+        #print self.isLeft(grid)
+
         # Visualise the environment grid
         cv2.imshow("Environment Grid", EnvironmentState.draw(grid))
 
@@ -76,7 +100,7 @@ class RandomAgent(Agent):
     def callback(self, learn, episode, iteration):
         """ Called at the end of each timestep for reporting/debugging purposes.
         """
-        if np.allclose(iteration % 500, 0): #show all specific
+        if iteration % 100 == 0: #show all specific
             print "{0}/{1}: {2}".format(episode, iteration, self.total_reward)
 
         # Show the game frame only if not learning
@@ -88,21 +112,16 @@ if __name__ == "__main__":
     seed = 16011984
     rng = np.random.RandomState(seed=seed)
     agent = RandomAgent(rng=rng)
-    #agent.run(True, episodes=2, draw=True)
-    size = 1
-    totalRewards = -1000 * np.ones(size, dtype=np.int) #minus zero to show that this is something invalid
 
-    for i in range(size):
-        agent.run(True, episodes=1, draw=True)
-        print 'Total reward: ' + str(agent.total_reward)
-        totalRewards[i] = agent.total_reward
+    agent.run(True, episodes=100, draw=True)
+    print 'Total reward: ' + str(agent.total_reward)
 
-    #print np.mean()
+    totalRewards, rewardStreams = agent.getRewardInfo()
+
     print totalRewards
-    print "Mean of Total Rewards: %f" % np.mean(totalRewards) #
-    print "Variance to Total Rewards: %f" % np.var(totalRewards) #
+    meanTotalRewards = np.mean(totalRewards)
+    varTotalRewards = np.var(totalRewards)
+    print "Mean of Total Rewards: %f" % meanTotalRewards
+    print "Variance to Total Rewards: %f" % varTotalRewards
 
-    #fig = plt.figure()
-    #plt.plot(np.arange(1, size+1), totalRewards)
-    #plt.show()
-    np.save('random_data', totalRewards)
+    np.savez('random_data', totalRewards, rewardStreams)
